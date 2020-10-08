47ac3eed65fa4db848bd0476318452aa79de64cb
Alexander Petrichenko <ptrch@ya.ru>
2020-10-08 13:57:29 +0300

    create all files for webpack

---
+++ webpack.config.js
@@ -0,0 +1,124 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // добавили плагин
+const webpack = require('webpack');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+        // test: /\.js$/, // include .js files
+        // enforce: "pre", // preload the jshint loader
+        // exclude: /node_modules/, // exclude any and all files in the `node_modules folder`
+        // use: [
+        //     {
+        //     loader: "babel-loader",
+        //       options: {
+        //         presets: [
+        //           "@babel/preset-env"
+        //         ],
+        //         plugins: [
+        //           "@babel/plugin-syntax-dynamic-import",
+        //           "@babel/plugin-proposal-class-properties"
+        //         ]
+        //       }
+        //     }
+        // ]
+      },
+      {
+        test: /\.css$/i,
+        use: [
+                        (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+                        {
+                          loader:'css-loader',
+                          options: {
+                              importLoaders: 2
+                          }
+                      },
+                        'postcss-loader'
+                ]
+      },
+     {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: "./images/[name].[ext]",
+              esModule: false
+            }
+        },
+       {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                optimizationLevel: 4,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+                },
+          ],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]'
+      },
+
+
+
+
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ //
+      filename: 'style.[contenthash].css',
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+              preset: ['default'],
+      },
+      canPrint: true
+    }),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash(),
+
+  ]
+
+};
